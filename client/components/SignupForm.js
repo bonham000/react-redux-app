0cc3debb3117b75726015967976dc32e0bc27b1c
Sean Smith <sean.smith.2009@gmail.com>
2016-10-17 12:58:33 -0500

    added client side uniqueness validation

--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -15,10 +15,12 @@ class SignupForm extends React.Component {
 			passwordConfirmation: '',
 			timezone: '',
 			errors: {},
-			isLoading: false
+			isLoading: false,
+			invalid: false
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.onSubmit = this.onSubmit.bind(this);
+		this.checkUserExists = this.checkUserExists.bind(this);
 	}
 	handleChange(e) {
 		this.setState({
@@ -34,6 +36,24 @@ class SignupForm extends React.Component {
 
 		return isValid;
 
+	}
+	checkUserExists(e) {
+		const field = e.target.name;
+		const val = e.target.value;
+		if (val !== '') {
+			this.props.isUserExists(val).then(res => {
+				let errors = this.state.errors;
+				let invalid;
+				if (res.data.user) {
+					errors[field] = 'There is user with such ' + field;
+					invalid = true;
+				} else {
+					errors[field] = '';
+					invalid = false;
+				}
+				this.setState({ errors, invalid });
+			});
+		}
 	}
 	onSubmit(e) {
 		e.preventDefault();
@@ -68,12 +88,14 @@ class SignupForm extends React.Component {
 					label = "Username"
 					onChange = {this.handleChange}
 					value = {this.state.username}
+					checkUserExists = {this.checkUserExists}
 					field = "username" />
 				<TextFieldGroup
 					error = {errors.email}
 					label = "Email"
 					onChange = {this.handleChange}
 					value = {this.state.email}
+					checkUserExists = {this.checkUserExists}
 					field = "email" />					
 				<TextFieldGroup
 					error = {errors.password}
@@ -104,7 +126,7 @@ class SignupForm extends React.Component {
 				</div>															
 															
 				<div className="form-group">
-					<button disabled = {this.state.isLoading} className="btn btn-primary btn-lg">Sign Up</button>
+					<button disabled = {this.state.isLoading || this.state.invalid} className="btn btn-primary btn-lg">Sign Up</button>
 				</div>
 			</form>
 		);
@@ -113,7 +135,8 @@ class SignupForm extends React.Component {
 
 SignupForm.propTypes = {
 	userSignupRequest: React.PropTypes.func.isRequired,
-	addFlashMessage: React.PropTypes.func.isRequired
+	addFlashMessage: React.PropTypes.func.isRequired,
+	isUserExists: React.PropTypes.func.isRequired
 }
 
 SignupForm.contextTypes = {
--- server/routes/users.js
+++ server/routes/users.js
@@ -30,6 +30,15 @@ function validateInput(data, otherValidations) {
 	})
 };
 
+router.get('/:identifier', (req, res) => {
+	User.query({
+		select: [ 'username', 'email' ],
+		where: { email: req.params.identifier },
+		orWhere: { username: req.params.identifier }
+	}).fetch().then(user => {
+		res.json({ user });
+	})
+});
 
 router.post('/', (req, res) => {
 
